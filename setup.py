cc367715908a1c6f632949a7b5e68981b6723b4c
Tonci Kokan <tonci@bloomandwild.com>
2020-10-25 14:35:17 +0000

    Add pipfiles and setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+import json
+
+install_requires = []
+
+with open("Pipfile.lock") as fd:
+    lock_data = json.load(fd)
+    install_requires = list(lock_data["default"].keys())
+
+setuptools.setup(
+    name="cactus",
+    version="0.1",
+    description="SQL formatter",
+    url="https://github.com/tkokan/cactus",
+    author="Tonci Kokan",
+    author_email="tkokan@gmail.com,
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    zip_safe=False
+)
