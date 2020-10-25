71f0318d8982492c1a1fa6112bc7a3e1d7853fed
Tonci Kokan <tonci@bloomandwild.com>
2020-10-25 12:47:58 +0000

    Work on entry point

--- cactus/main.py
+++ cactus/main.py
@@ -77,7 +77,82 @@ def parse_args(argv: Optional[Sequence[str]] = None) -> Dict[str, Any]:
 
 def main(argv: Optional[Sequence[str]] = None) -> None:
     arguments = parse_args(argv)
-    pass
+    
+    file_names = arguments.pop("files", [])
+    
+    config_dict = arguments.copy()
+    check = config_dict.pop("check", False)
+
+    skipped: List[str] = []
+    broken: List[str] = []
+
+    file_names = iter_source_code(file_names, config, skipped, broken)
+
+    num_skipped = 0
+    num_broken = 0
+    num_invalid_encoding = 0
+
+    attempt_iterator = (
+        sort_imports(
+            file_name,
+            config=config,
+            check=check,
+            ask_to_apply=ask_to_apply,
+            show_diff=show_diff,
+            write_to_stdout=write_to_stdout,
+        )
+        for file_name in file_names
+    )
+
+    # If any files passed in are missing considered as error, should be removed
+    is_no_attempt = True
+    any_encoding_valid = False
+    for sort_attempt in attempt_iterator:
+        if not sort_attempt:
+            continue  # pragma: no cover - shouldn't happen, satisfies type constraint
+        incorrectly_sorted = sort_attempt.incorrectly_sorted
+        if arguments.get("check", False) and incorrectly_sorted:
+            wrong_sorted_files = True
+        if sort_attempt.skipped:
+            num_skipped += (
+                1  # pragma: no cover - shouldn't happen, due to skip in iter_source_code
+            )
+
+        if not sort_attempt.supported_encoding:
+            num_invalid_encoding += 1
+        else:
+            any_encoding_valid = True
+
+        is_no_attempt = False
+
+    num_skipped += len(skipped)
+    if num_skipped and not arguments.get("quiet", False):
+        if config.verbose:
+            for was_skipped in skipped:
+                warn(
+                    f"{was_skipped} was skipped as it's listed in 'skip' setting"
+                    " or matches a glob in 'skip_glob' setting"
+                )
+        print(f"Skipped {num_skipped} files")
+
+    num_broken += len(broken)
+    if num_broken and not arguments.get("quite", False):
+        if config.verbose:
+            for was_broken in broken:
+                warn(f"{was_broken} was broken path, make sure it exists correctly")
+        print(f"Broken {num_broken} paths")
+
+    if num_broken > 0 and is_no_attempt:
+        all_attempt_broken = True
+    if num_invalid_encoding > 0 and not any_encoding_valid:
+        no_valid_encodings = True
+
+    if wrong_sorted_files:
+        sys.exit(1)
+
+    if all_attempt_broken:
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
